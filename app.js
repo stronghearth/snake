fed1ad3d37a84ee1c6553228134c83c35ae3fa98
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-06-08 14:19:32 -0400

    wrote control function

--- app.js
+++ app.js
@@ -13,4 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
     let speed = 0.9
     let intervalTime = 0
     let interval = 0
+
+    function control(e) {
+        squares[currentIndex].classList.remove('snake')
+
+        if(e.keyCode === 39) {
+            direction = 1
+        } else if(e.keyCode === 38) {
+            direction = -width
+        } else if(e.keyCode === 37) {
+            direction -1
+        } else if(e.keyCode === 40) {
+            direction = +width
+        }
+    }
 })
