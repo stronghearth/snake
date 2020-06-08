3e7bf3c4cb401a0b6656308676b07293a4c5cff4
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-06-08 14:15:58 -0400

    set up variables for js

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const scoreDisplay = document.querySelector('span')
+    const startBtn = document.querySelector('.start')
+
+    const width = 10
+    let currentIndex = 0
+    let appleIndex = 0
+    let currentSnake = [2,1,0] //2 head, 0 tail of snake
+
+    let direction = 1
+    let score = 0
+    let speed = 0.9
+    let intervalTime = 0
+    let interval = 0
+})
